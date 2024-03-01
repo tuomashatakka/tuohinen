7f2a9a40f991d430f6063a456b5b5b0820488532
Tuomas Hatakka <tuomas.hatakka@gmail.com>
2024-03-01 13:16:14 +0200

    Change background color

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,11 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      backgroundColor: {
+        red: '19',
+        green: '18',
+        blue: '18'
+      }
     },
   },
   plugins: [],
