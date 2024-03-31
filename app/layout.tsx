c0ab05fdc38f80a944b5f9fe03088801a369c734
Tuomas Hatakka <tuomas.hatakka@gmail.com>
2024-03-31 19:56:50 +0300

    fdgjklghifughkojgvpöodih oitgdkfölmjhbgnfgj

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,9 +4,11 @@ import Navigation from './_components/Navigation'
 import PageHeadSection from './_components/PageHeadSection'
 import { NavigationProvider } from './_components/NavigationContext'
 import { classNameWithFont } from '@/theme/fonts'
+import { Analytics } from '@vercel/analytics/react'
 
 import '@/styles/globals.css'
 import 'material-icons/iconfont/material-icons.css'
+import metadata from '@/lib/metadata'
 
 
 const className = classNameWithFont('epilogue')
@@ -15,6 +17,9 @@ export default function RootLayout ({ children }: RootLayoutProps) {
   const bodyClassName = className()
 
   return <html lang='en'>
+    <head>
+      <title>{ metadata.title }</title>
+    </head>
     <body className={bodyClassName}>
       <PageHeadSection />
       <div className='main-content-wrapper'>
@@ -22,6 +27,7 @@ export default function RootLayout ({ children }: RootLayoutProps) {
           <Navigation />
           <main className='main-content min-h-screen'>
             { children }
+            <Analytics />
           </main>
         </NavigationProvider>
       </div>
