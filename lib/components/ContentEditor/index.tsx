5ca580be86733121d79c09c1539c4ad823af4dec
Tuomas Hatakka <tuomas.hatakka@gmail.com>
2024-04-15 19:53:34 +0300

    Add ts error ignore for wysiwyg editor's onError handler

--- lib/components/ContentEditor/index.tsx
+++ lib/components/ContentEditor/index.tsx
@@ -14,6 +14,7 @@ const theme = {
 // Catch any errors that occur during Lexical updates and log them
 // or throw them as needed. If you don't throw them, Lexical will
 // try to recover gracefully without losing user data.
+// @ts-expect-error TODO: error type
 function onError (error) {
   // eslint-disable-next-line no-console
   console.error(error)
