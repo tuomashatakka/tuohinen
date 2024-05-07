f4614d6090a6e32a52c74ec9906d3451a515404f
Tuomas Hatakka <tuomas.hatakka@gmail.com>
2024-05-07 13:57:04 +0300

    Update Artisti & Tunnelma section content

--- app/_components/Segments.tsx
+++ app/_components/Segments.tsx
@@ -138,24 +138,47 @@ export const Ekologisuus = () => <Segment variant={ SegmentVariantType.light } t
 export const Artisti = () => <Segment variant={ SegmentVariantType.dark } title='Artisti'>
   {(Column) => <>
     <Column padding width={ 3 } minWidth={ ScreenSize.lg }>
-      <H2>Artisti</H2>
+      <H2>Artistista, Tompurista</H2>
       <Ingress>
-          Tompuri on koulutukseltaan kirvesmies. Metallihommia telakalla ja tehtailla.
-          Helsingissä varttunut taiteilijasielu.
+        Helsingissä varttunut Tompuri on koulutukseltaan kirvesmies.
+        Hän on vuosia rehkinyt käsityöläisen hommien parissa; metallihommia telakalla ja tehtailla.
       </Ingress>
-      <Paragraph>
-          Tompuri toimi tatuointiartistina vuosia. Kaiken ohella hän on aina tehnyt taidetta.
-          Aidon käsityöläisen kädenjälki näkyy tuohisen tuotteissa.
-      </Paragraph>
-      <Paragraph>
-          Taiteilija Otto Mikael Tompuri haluaa antaa estradin kansallispuullemme koivulle
-          ja sen pelkistetylle kauneudelle.
-      </Paragraph>
-
+      <Ingress>
+        Aidon käsityöläisen kädenjälki näkyy tuohisen tuotteissa.
+      </Ingress>
       <Spacer align='right' />
-      <H3>Meri & luonto</H3>
-      <Paragraph>
 
+      <Paragraph>
+        Metallihommien ohessa Tompuri on toteuttanut itseään tatuointiartistina ja
+        graafisen taiteen luojana yli vuosikymmenen ajan.
+        Tatuoiminen on Tompurille yksi taiteen muoto muiden joukossa.
+        Kuin maalaaminen, mutta elävällä kanvaksella.
+      </Paragraph>
+      <Ingress>
+        Itsensä toteuttaminen taiteen keinoin on aina kulkenut elämässä mukana, tilanteessa kuin tilanteessa.
+      </Ingress>
+      <Spacer align='right' />
+
+      <H3>Artisti ja artesaani</H3>
+      <Paragraph>
+        Taiteilija Otto Mikael Tompuri haluaa antaa estradin kansallispuullemme koivulle
+        ja sen pelkistetylle kauneudelle. Valmistetut valaisimet on loihdittu yhdistämällä useamman vuosikymmenen kokemus
+        toisaalta käytännön käsityöläisenä ja toisaalta luovana artistisieluna.
+      </Paragraph>
+
+      <H3>Tompuri, maailman matkaaja</H3>
+      <Paragraph>
+        Tompuri on kiertänyt useita kertoja ympäri Aasiaa ja saanut usein kuulla paikallisilta kommentin:
+        <blockquote>Et vaikuta yhtään Suomalaiselta ihmiseltä, haha.</blockquote>
+        Kyllä, tiedän, Tompuri on aina tällöin miettinyt.
+      </Paragraph>
+      <Paragraph>
+        Tompuri rakastaa Aasialaista kulttuuria, etenkin Indonesiaa.
+        Hänellä onkin rutkasti ystäviä - ja koti - sekä Suomessa että Sumatralla.
+        Viime visiitillään Aasiassa hän vietti aikaa pienellä saarella Malaysiassa, <em>Tiomanissa</em>.
+        Siellä paikallinen ravintolooitsijaystävä ehdotti hänelle, josko hän
+        jäisi asumaan saarelle - oman studion voisi rakentaa aivan ravintolan viereen.
+        Vaikka idea oli houkutteleva, Tompuri halusi palata Suomeen, juurilleen.
       </Paragraph>
     </Column>
     <Column width={ 2 } minWidth={ ScreenSize.sm }>
@@ -171,8 +194,38 @@ export const Tunnelma = () => <Segment variant={ SegmentVariantType.light } titl
     <Column padding width={ 3 } minWidth={ ScreenSize.lg }>
       <H2>Taide & tunnelma</H2>
       <Ingress>
-          Valaisimien saatavuus on täysin kytköksissä materiaalien saatavuuteen.
+        On sitä huonoakin tuohta. Ja paljonkin...
       </Ingress>
+      <Paragraph>
+        Valaisimien saatavuus on täysin kytköksissä materiaalien saatavuuteen.
+        Koko prosessi on pitkä ja kivinen. aina materiaalien hankkimisesta valaisimien viimeistelyyn asti.
+        Ei ole lainkaan itsestäänselvyys, että hyvää, kaunista ja laadukasta tuohiaihiota löytyy lainkaan -
+        etsiminen voi kestää päiviä, vaikka hyvä apaja olisikin tiedossa.
+      </Paragraph>
+      <Paragraph>
+        Tompuri on vuosien ajan tutkinut luontoa, etenkin koivun tuohta ja siihen vaikuttavia tekijöitä.
+        Koivun tuohta tutkiessa on valehtelematta kierretty koko Suomi:
+        Lapissa varhaiskoivuja lumikengillä samoillen, Etelä-Suomessa ympäri saaristoja veneillen.
+      </Paragraph>
+
+      <H3>Luontoa pitää osata lukea</H3>
+      <Paragraph>
+        Kuten valaisimien saatavuus on kytköksissä materiaalien saatavuuteen,
+        materiaalien saatavuus puolestaan on täysin kytköksissä siihen mitä luonto antaa.
+        Esimerkiksi ulkoisesti hyvännäköinen lahopuu saattaa olla sammaleen puolelta täysin mätä;
+        usean vuoden mättäillä makaaminen on tehnyt tehtävänsä, kuten luonnon kuuluukin -
+        lahopuilla on tärkeä rooli luonnon monimuotoisuuden ylläpitämisessä.
+      </Paragraph>
+      <Paragraph>
+        Pyrkimys on, että mitään ei oteta turhaan - jos edes otetaan.
+        "En itsekään mene kalaan ja riistä sen henkeä jos en aio syödä", Tompuri tiivistää.
+        "Luontoa ja sen herkkiä prosesseja haluan vaalia ja varjella viimeiseen asti."
+      </Paragraph>
+      <Paragraph>
+        Kun hyvä tuohiaihio vihdoin löytyy, materiaali pestään ja puhdistetaan heti,
+        alustavasti paikan päällä ja huolellisemmin sen kuljettua varastoon.
+        Kaikki ylimääräinen jätetään luontoon myös tässä mielessä.
+      </Paragraph>
     </Column>
     <Column width={ 2 } minWidth={ ScreenSize.sm }>
       <LightboxItem id='photoPhotoshopMadeMeDoIt' description=''>
