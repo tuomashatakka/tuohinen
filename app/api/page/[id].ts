ea65ebf6288f48451061079deebb40efc5695de7
Tuomas Hatakka <tuomas.hatakka@gmail.com>
2024-06-05 21:02:39 +0300

    Disable error for api file

--- app/api/page/[id].ts
+++ app/api/page/[id].ts
@@ -4,5 +4,8 @@ import { NextResponse } from 'next/server'
 // eslint-disable-next-line import/prefer-default-export
 export async function GET (request: Request, response: NextResponse) {
   const result = await prisma.page.findMany()
+
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-expect-error
   response.json(result)
 }
