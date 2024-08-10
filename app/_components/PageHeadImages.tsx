8e99dc5de8794d633c6d7e61c7d9ea5a09c609d9
Tundra Hatakka <tuomas.hatakka@gmail.com>
2024-08-10 22:12:35 +0300

    Update heading images

--- app/_components/PageHeadImages.tsx
+++ app/_components/PageHeadImages.tsx
@@ -3,7 +3,7 @@ import lukee from '@/public/hero/lukia.jpg'
 import tunnelmoi from '@/public/hero/chill.jpg'
 import istuu from '@/public/hero/moottorisaha.jpg'
 import yopoyta from '@/public/hero/yopoyta.jpg'
-import puu from '@/public/hero/puu.jpg'
+import puu from '@/public/hero/lamput.jpeg'
 
 const slides = [
   lukee,
