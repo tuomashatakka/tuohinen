e330fe8b78850a82d1a792eeefccae914c89a8bb
Tuomas Hatakka <tuomas.hatakka@gmail.com>
2025-01-26 20:52:15 +0200

    Add subpage to the header title

--- app/_components/PageHeadSection.tsx
+++ app/_components/PageHeadSection.tsx
@@ -1,22 +1,26 @@
-import { Subtitle, Title } from '@/components/Type'
+'use client'
+import { Title } from '@/components/Type'
 import ScrollDownButton from './ScrollDownButton'
 import CarouselComponent from '@/lib/components/Carousel'
 
 import slides from './PageHeadImages'
 import style from './PageHeadSection.module.sass'
+import { useActivePage } from './Navigation'
 
 
 export default function PageHeadSection () {
+  const page = useActivePage()
+
   return <section className='banner min-h-screen animate-blur-in'>
     <CarouselComponent className='h-100 h-screen' slides={ slides } />
 
     <header className={ style.heading_content }>
-      <Subtitle>
-        Tuohinen tunnelmavalaisimet
-      </Subtitle>
-
       <Title>
-        tuo luonto kotiisi.
+        <span className={ style.title }>Tuohinen tunnelmavalaisimet</span>
+        <span className={ style.slogan }>tuo luonto kotiisi.</span>
+        { page?.id !== 'etusivu'
+          ? <span className={ style.subtitle }>{ page?.text }</span>
+          : null }
       </Title>
     </header>
 
